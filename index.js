b70c0ecc743965574fbcb8007293e5f3da5c840e
Steve Kariuki <stevekariuki5@gmail.com>
2022-12-05 02:10:34 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let message = [];
+    for (let counter = 0; counter < name.length; counter++) {
+        console.log(`Thank you, ${name[counter]}, for the wonderful ${event} gift!`);
+        message[counter] = `Thank you, ${name[counter]}, for the wonderful ${event} gift!`;
+    }
+    return message;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
